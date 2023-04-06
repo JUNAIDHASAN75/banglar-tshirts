681f95a5e6789242467e91ecfd1a24edf41736bf
JUNAIDHASAN75 <spiderss907@gmail.com>
2023-04-06 22:31:13 +0600

    initial data load

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.css'
+
+const Header = () => {
+    return (
+        <nav>
+            <h2>this is header compo</h2>
+            <Link to="/">Home</Link>
+            <Link to="/review">Order Review</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
+    );
+};
+
+export default Header;
